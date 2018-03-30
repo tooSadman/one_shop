551eea09373b283f663fd6ff05ad47648c02f971
Nikita Kiselov <kinivi210999@gmail.com>
2018-03-30 16:35:23 +0300

    Hot fix for Bug with GlobalKey

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -14,6 +14,8 @@ class HomePage extends StatefulWidget {
 // This widget is the home page of your application. It is stateful, meaning
 // that it has a State object (defined below) that contains fields that affect
 // how it looks.
+
+  //TODO try to use builder
   static final homePageScaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
